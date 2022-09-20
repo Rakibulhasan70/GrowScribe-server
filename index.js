9aa6036fd241519595680f3d07c2d1afa598251a
Rakibulhasan70 <rakibulhasansohag5@gmail.com>
2022-09-20 12:47:38 +0600

    env is done

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://easydoc:jcoMfnZUSQBKBNOI@cluster0.faflb.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.faflb.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
