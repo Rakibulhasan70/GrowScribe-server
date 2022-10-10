740a86904908d166c18d4735bc1bf941ae338640
Rakibulhasan70 <rakibulhasansohag5@gmail.com>
2022-10-10 12:36:36 +0600

    done

--- index.js
+++ index.js
@@ -18,18 +18,104 @@ async function run() {
         await client.connect()
         const inputCollection = client.db('EasyDoc').collection('react')
 
-        app.post('/input', async (req, res) => {
-            const news = req.body
-            const result = await inputCollection.insertOne(news)
+        const studentCollection = client.db('EasyDoc').collection('student')
+
+        const subjectCollection = client.db('EasyDoc').collection('student-subject')
+
+        app.post('/add', async (req, res) => {
+            const bio = req.body
+            const result = await studentCollection.insertOne(bio)
             res.send(result)
         })
 
-        app.get('/inputs', async (req, res) => {
+
+        app.delete('/addDelete/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await studentCollection.deleteOne(query)
+            res.send(result)
+        })
+
+
+        app.get('/shows', async (req, res) => {
             const query = {}
-            const cursor = inputCollection.find(query)
+            const cursor = studentCollection.find(query)
             const result = await cursor.toArray()
             res.send(result)
         });
+
+
+        app.put('/shows/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatUser = req.body;
+            const filter = { _id: ObjectId(id) }
+            const option = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    firstName: updatUser.firstName,
+                    lastName: updatUser.lastName,
+                    date: updatUser.date,
+                    age: updatUser.age,
+                    city: updatUser.city,
+                    skill: updatUser.skill,
+                    EnrollmentFrom: updatUser.EnrollmentFrom,
+                    EnrollmentTo: updatUser.EnrollmentTo,
+                    status: updatUser.status,
+                    Standard: updatUser.Standard,
+                    brief: updatUser.brief,
+                }
+            };
+            const result = await studentCollection.updateOne(filter, updateDoc, option)
+            res.send(result);
+        });
+
+
+        app.get('/shows/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await studentCollection.findOne(query);
+            res.send(result)
+        });
+
+        // //////////////////////////////////////////////////////////////////////////////
+
+
+        app.get('/show', async (req, res) => {
+            const query = {}
+            const cursor = subjectCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        });
+
+
+        app.delete('/subjectAdd/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await subjectCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        app.post('/subjectAdd', async (req, res) => {
+            const bio = req.body
+            const result = await subjectCollection.insertOne(bio)
+            res.send(result)
+        })
+
+
+
+
+        // app.post('/input', async (req, res) => {
+        //     const news = req.body
+        //     const result = await inputCollection.insertOne(news)
+        //     res.send(result)
+        // })
+
+        // app.get('/inputs', async (req, res) => {
+        //     const query = {}
+        //     const cursor = inputCollection.find(query)
+        //     const result = await cursor.toArray()
+        //     res.send(result)
+        // });
 
     }
     finally {
